984160306098771c881fe678de60916bfe67c31a
wirukom <wirukom@gmail.com>
2018-05-02 17:02:22 +0900

    テストプロジェクト追加

---
+++ AppDelegate.cs
@@ -0,0 +1,65 @@
+﻿using Foundation;
+using UIKit;
+
+namespace test
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        // class-level dieclaratons
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            //アプリケーション起動完了後にOverrride point をカスタマイズできる
+            //このメソッドは不要であれば削除可能
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+            return true;
+        }
+
+        public override void OnResignActivation(UIApplication application)
+        {
+            // Invoked when the application is about to move from active to inactive state.
+            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+            // or when the user quits the application and it begins the transition to the background state.
+            // Games should use this method to pause the game.
+        }
+
+        public override void DidEnterBackground(UIApplication application)
+        {
+            //バックグラウンドエクスプレッションの場合、アプリ終了時にWillTerminateの代わりとして呼び出される
+            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
+            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
+        }
+
+        public override void WillEnterForeground(UIApplication application)
+        {
+            //背景からアクティブ状態へ遷移する時に呼び出される
+            // Called as part of the transiton from background to active state.
+            // Here you can undo many of the changes made on entering the background.
+        }
+
+        public override void OnActivated(UIApplication application)
+        {
+            //タスクの再起動
+            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
+            // If the application was previously in the background, optionally refresh the user interface.
+        }
+
+        public override void WillTerminate(UIApplication application)
+        {
+            //アプリ終了時に呼び出される
+            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+        }
+    }
+}
+
